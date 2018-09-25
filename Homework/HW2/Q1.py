9c4c8dc32bb236af12201dea30600a4a0c9b9b70
Palacio, Nick <nick.palacio@nngco.com>
2018-09-25 15:55:27 -0500

    Working out bugs

--- Homework/HW2/Q1.py
+++ Homework/HW2/Q1.py
@@ -8,13 +8,31 @@ CONSTANTS = {
     "RangeOfStateValues": 8
 }
 
+def SinglePointCrossover(options):
+    # Expected params
+    parent1AsList = list(options["parent1"])
+    parent2AsList = list(options["parent2"])
+
+    crossoverPoint = random.randint(1, CONSTANTS["RangeOfStateValues"] - 1)
+    # Child 1 = [0]..(crossoverPoint) = parent1, crossoverPoint..[7] = parent2
+    child1 = parent1AsList[0:crossoverPoint] + parent2AsList[crossoverPoint:CONSTANTS["RangeOfStateValues"]]
+    # Child 2 = [0]..(crossoverPoint) = parent2, crossoverPoint..[7] = parent1
+    child2 = parent2AsList[0:crossoverPoint] + parent1AsList[crossoverPoint:CONSTANTS["RangeOfStateValues"]]
+
+    return ["".join(child1),"".join(child2)]
+
 def ReproduceWithParents(options):
     # Expected params
+    parent1 = options["parent1"]
+    parent2 = options["parent2"]
     crossoverOperator = options["crossoverOperator"]
 
     # Should return pair of children as array of strings
     if crossoverOperator == 0:
-        print("Implement singlePoint crossover")
+        return SinglePointCrossover({
+            "parent1": parent1,
+            "parent2": parent2
+        })
     elif crossoverOperator == 1:
         print("Implement twpoPoint crossover")
     elif crossoverOperator == 2:
@@ -23,9 +41,15 @@ def ReproduceWithParents(options):
         print("Implement uniform crossover")
 
 def GetGoalStateChild(options):
-    print("Implement GetGoalStateChild")
+    # Expected params
+    childrenWithFitness = options["childrenWithFitness"]
+
     # Return none if no goal state children
     # Otherwise return just the child (state)
+    for child in childrenWithFitness:
+        if child["fitness"] == CONSTANTS["GoalFitness"]:
+            return child["child"]
+    return None
 
 def CalculateFitnesses(options):
     # Expected params
@@ -92,9 +116,17 @@ def GetDiagonalAttackers(options):
 
 
 def AddChildrenToQueue(options):
-    print("Implement AddChildrenToQueue")
-    # heapq.heappush(options["population"], (options["childFitness"], popSize + 1, options["child"]))
+    # Expected params
+    childrenWithFitness = options["childrenWithFitness"]
+    population = options["population"]
+
     # Should return updated list (queue)
+    popSize = len(population)
+    currIndex = popSize + 1
+    for child in childrenWithFitness:
+        heapq.heappush(population, (child["fitness"], currIndex, child["child"]))
+        currIndex += 1
+    return population
 
 def Fitness(state):
     stateAsList = list(state)
@@ -135,30 +167,51 @@ def Mutate(options):
 
     # Should mutate a set of children and return new list
 
-def GetParentByDistribution(options):
+def GetParentIndexByDistribution(options):
     # Expected params
     popWithDistr = options["populationWithDistribution"]
 
     randomNum = random.randint(0,100000) / 100000
     # Could I keep adding up the children percentages until I go higher than the random number?
     currSum = 0
-    for child in popWithDistr:
-        currChild = child["child"]
-        currSum += child["distribution"]
+    # for child in popWithDistr:
+    for i in range(len(popWithDistr)):
+        currChild = popWithDistr[i]
+        currSum += currChild["distribution"]
         if currSum > randomNum:
-            return currChild
+            return i
 
 def GetParentsByDistribution(options):
     # Expected params
     chromosomesPerIteration = options["chromosomesPerIteration"]
-    popWithDistr = options["populationWithDistribution"]
+    # popWithDistr = options["populationWithDistribution"].copy()
+    population = options["population"].copy()
+
+    populationWithDistribution = GetChildrenWithDistribution({
+        "population": population
+    })
 
     parents = []
     for i in range(chromosomesPerIteration):
-        parents.append(GetParentByDistribution({
+        newParentIndex = GetParentIndexByDistribution({
             "populationWithDistribution": popWithDistr
-        }))
+        })
+        parents.append(popWithDistr[newParentIndex])
+        # Once I get a parent I need to make sure I dont select it again
+        del population[newParentIndex]
+        # Need to recalculate distribution everytime
+        populationWithDistribution = GetChildrenWithDistribution({
+            "population": population
+        })
     return parents
+
+def GetChildrenWithDistribution(options):
+    # Expected params
+    population = options["population"]
+    
+    sumOfFitnessValues = sum([x[0] for x in population])
+    # Get sum of all fitness values, use this to calculate each ones distribution
+    return [{"child": x[2], "distribution": x[0] / sumOfFitnessValues} for x in population]
 
 def GetParents(options):
     # Expected params
@@ -167,11 +220,15 @@ def GetParents(options):
 
     # Should return n parents probabalistically based on fitness
     # Select n parents based on probabilistic distribution of their fitness functions
-    sumOfFitnessValues = sum([x[0] for x in population])
+    # sumOfFitnessValues = sum([x[0] for x in population])
     # Get sum of all fitness values, use this to calculate each ones distribution
-    populationWithDistribution = [{"child": x["2"], "distribution": x[0] / sumOfFitnessValues} for x in population]
+    # populationWithDistribution = [{"child": x[2], "distribution": x[0] / sumOfFitnessValues} for x in population]
+    # populationWithDistribution = GetChildrenWithDistribution({
+    #     "population": population
+    # })
     parents = GetParentsByDistribution({
-        "populationWithDistribution": populationWithDistribution,
+        "population": population,
+        # "populationWithDistribution": populationWithDistribution,
         "chromosomesPerIteration": chromosomesPerIteration
     })
     return parents
@@ -197,8 +254,38 @@ def GenerateChildren(options):
     })
     return children
 
+def GetCrossoverOperatorName(crossoverOperator):
+    if crossoverOperator == 0:
+        return "Single Point"
+    elif crossoverOperator == 1:
+        return "Two Point"
+    elif crossoverOperator == 2:
+        return "Cut and splice"
+    elif crossoverOperator == 3:
+        return "Uniform"
+
 def PrintSolution(options):
-    print("Implement print solution")
+    # Expected params
+    finalState = options["solution"]["finalState"]
+    finalStateFitness = options["solution"]["finalStateFitness"]
+    iterations = options["solution"]["iterations"]
+    initialPopSize = len(options["parameters"]["initialPopulation"])
+    chromosomesPerIteration = options["parameters"]["chromosomesPerIteration"]
+    mutationRate = options["parameters"]["mutationRate"]
+    maxIterations = options["parameters"]["maxIterations"]
+    crossoverOperator = options["parameters"]["crossoverOperator"]
+
+    print("Solution found with the following parameters:" + os.linesep)
+    parameterString = "Initial Population Size: " + str(initialPopSize)
+    parameterString += ", Chromosomes generated per iteration: " + str(chromosomesPerIteration)
+    parameterString += ", MutationRate: " + str(mutationRate)
+    parameterString += ", Max Iterations: " + str(maxIterations)
+    parameterString += ", Crossover operator used: " + GetCrossoverOperatorName(crossoverOperator)
+    print(parameterString)
+
+    print("Iterations used: " + str(iterations))
+    print("Final State: " + str(finalState))
+    print("Final State Fitness: " + str(finalStateFitness))
 
 def GetSolution(options):
     # Expected params
@@ -206,12 +293,13 @@ def GetSolution(options):
     chromosomesPerIteration = options["chromosomesPerIteration"]
     crossoverOperator = options["crossoverOperator"]
     maxIterations = options["maxIterations"]
+    mutationRate = options["mutationRate"]
 
     # Should be a priority Q (fitness, dumbCounter, node)
     isIterationLimit = maxIterations != -1
     iterationCount = 0
     # loop either # of iterations or until goal state is reached
-    while true:
+    while True:
         # How do I select what 2 states to reproduce with?
         # Get the top 2 from pop based on their fitness function values
         children = GenerateChildren({
@@ -220,80 +308,123 @@ def GetSolution(options):
             "crossoverOperator": crossoverOperator,
             "mutationRate": mutationRate
         })
+        childrenWithFitness = CalculateFitnesses({
+            "children": children
+        })
         # If maxIterations = -1, check if child is goal state and return it if it is
         if isIterationLimit:
             if iterationCount >= maxIterations:
                 # We are done iterating, return our best
-                return heapq.heappop(population)[2]
-            iterationCount += 1
+                finalState = heapq.heappop(population)[2]
+                return {
+                    "finalState": finalState,
+                    "finalStateFitness": Fitness(finalState),
+                    "iterations": iterationCount
+                }
         else:
             # Only return when our child is a goal state
-            childrenWithFitness = CalculateFitnesses({
-                "children": children
-            })
             goalStateChild = GetGoalStateChild({
                 "childrenWithFitness": childrenWithFitness
             })
             if goalStateChild != None:
-                return goalStateChild
-            else:
-                # Add child to q
-                population = AddChildrenToQueue({
-                    "childrenWithFitness": childrenWithFitness,
-                    "population": population
-                })
+                # return goalStateChild
+                return {
+                    "finalState": goalStateChild,
+                    "finalStateFitness": CONSTANTS["GoalFitness"],
+                    "iterations": iterationCount
+                }
+        # Always need to add children to q if we are continuing
+        population = AddChildrenToQueue({
+            "childrenWithFitness": childrenWithFitness,
+            "population": population
+        })
+        iterationCount += 1
 
 def GetInitialPopulation(options):
-    # TODO: Make sure that no one in initial pop is goal state
-    print("Implement get initial pop")
+    # Expected params
+    size = options["initialSize"]
+
+    population = []
+    populationSize = 0
+    while populationSize < size:
+        stateAsList = []
+        for i in range(CONSTANTS["LengthOfState"]):
+            stateAsList.append(str(random.randint(1,CONSTANTS["RangeOfStateValues"])))
+        state = "".join(stateAsList)
+        stateFitness = Fitness(state)
+        if stateFitness < CONSTANTS["GoalFitness"]:
+            # heapq.heappush(population, (child["fitness"], currIndex, child["child"]))
+            heapq.heappush(population, (stateFitness, populationSize, state))
+            # population.append(state)
+            populationSize += 1
+    return population
 
 def Solve(options):
+    initialPopSize = options["initialPopulationSize"]
+
     # Get initial population
     initialPop = GetInitialPopulation({
-        "initialSize": options["initialPopulationSize"]
+        "initialSize": initialPopSize
     })
 
     # Get solution
-    solution = GetSolution({
+    parameters = {
         "initialPopulation": initialPop,
         "chromosomesPerIteration": options["chromosomesPerIteration"],
         "mutationRate": options["mutationRate"],
         "maxIterations": options["maxIterations"],
         "crossoverOperator": options["crossoverOperator"]
-    })
+    }
+    solution = GetSolution(parameters)
 
     # Print solution
     PrintSolution({
-        "solution": solution
+        "solution": solution,
+        "parameters": parameters
     })
 
-def GetAlgorithm():
+def GetCrossoverOperator():
     algorithm = input("Please specify crossover operator:" + os.linesep + "Single point = 0" + os.linesep + "Two point = 1" + os.linesep + "Cut and splice = 2" + os.linesep + "Uniform = 3" + os.linesep)
     return int(algorithm)
 
 def GetInputs():
-    # TODO: chromosomesPerIteration needs to be even
-    # TODO: mutation rate must be <1
     inputs = input("Please provide the following parameters separated by a space: <Initial population size> <Number of chromosomes to select per iteration> <Mutation Rate> <Number of iterations to run for>:" + os.linesep)
     inputs = inputs.split()
+    initialPopSize = int(inputs[0])
+    chromPerIteration = int(inputs[1])
+    mutationRate = float(inputs[2])
+    if chromPerIteration % 2 != 0:
+        raise Exception("Chromosome per iteration value must be even so we have pairs of parents to generate them")
+    if initialPopSize < chromPerIteration:
+        raise Exception("Initial population size cannot be smaller than the number of chromosomes to generate per iteration")
+    if mutationRate > 1:
+        raise Exception("Mutation rate must be <1")
     return {
-        "initialPopulationSize": int(inputs[0]),
-        "chromosomesPerIteration": int(inputs[1]),
-        "mutationRate": float(inputs[2]),
+        "initialPopulationSize": initialPopSize,
+        "chromosomesPerIteration": chromPerIteration,
+        "mutationRate": mutationRate,
         "maxIterations": int(inputs[3])
     }
 
 def Main():
-    # Prompt user for inputs
-    inputs = GetInputs()
-    crossoverOperator = GetCrossoverOperator()
-    Solve({
-        "initialPopulationSize": inputs["initialPopulationSize"],
-        "chromosomesPerIteration": inputs["chromosomesPerIteration"],
-        "mutationRate": inputs["mutationRate"],
-        "maxIterations": inputs["maxIterations"],
-        "crossoverOperator": crossoverOperator
-    })
+    willContinue = True
+    while willContinue:
+        # Prompt user for inputs
+        inputs = GetInputs()
+        crossoverOperator = GetCrossoverOperator()
+        Solve({
+            "initialPopulationSize": inputs["initialPopulationSize"],
+            "chromosomesPerIteration": inputs["chromosomesPerIteration"],
+            "mutationRate": inputs["mutationRate"],
+            "maxIterations": inputs["maxIterations"],
+            "crossoverOperator": crossoverOperator
+        })
+        willContinue = input("Type Y to try another scenario") == "Y"
 
 # Fitness("32752411")
 # Fitness("24748552")
+# SinglePointCrossover({
+#     "parent1": "32543213",
+#     "parent2": "31234523"
+# })
+Main()
