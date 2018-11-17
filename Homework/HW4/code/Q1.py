efef2f5aa1b51c1cb5d26a856d0791aa519e7389
Nick Palacio <npalacio2240@gmail.com>
2018-11-16 20:27:08 -0600

    dropped print statement

--- Homework/HW4/code/Q1.py
+++ Homework/HW4/code/Q1.py
@@ -257,7 +257,7 @@ def Main(actualTransitionDict, discountFactor, epochLimit, gridInfos):
         actualRewardDict = GetActualRewardDict(gridInfo)
         # Get policy for this world
         policy = GetPolicy(actualRewardDict, actualTransitionDict, discountFactor, gridInfo)
-        PrintPolicy(policy, gridInfo)
+        # PrintPolicy(policy, gridInfo)
         newPolicy = None
         # print('Algorithm starting at ' + time.strftime("%H:%M:%S"))
         if algorithm == 1:
