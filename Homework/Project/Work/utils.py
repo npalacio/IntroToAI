82275f2efe8be904c4e8615fab9cbef7c1c59ff1
Nick Palacio <npalacio2240@gmail.com>
2018-12-03 19:17:17 -0600

    backing up work

--- Homework/Project/Work/utils.py
+++ Homework/Project/Work/utils.py
@@ -1,5 +1,6 @@
 import math
 from datetime import datetime
+from datetime import timedelta
 
 def FilterColumns(df, columns):
     return df.filter(items=columns)
@@ -14,17 +15,43 @@ def ConvertToNumeric(row, column, valuesToHandle, defaultValue):
         value = defaultValue
     return float(value)
 
+def CalculateWeatherDataYear(row):
+    dateTime = GetDateTime(row)
+    return dateTime.year
+
+def CalculateWeatherDataMonth(row):
+    dateTime = GetDateTime(row)
+    return dateTime.month
+
+def CalculateWeatherDataDay(row):
+    dateTime = GetDateTime(row)
+    return dateTime.day
+
 def CalculateWeatherDataHour(row):
-    year = str(row['Year'])
-    month = '%02d' % int(row['Month'])
-    day = '%02d' % int(row['Day'])
+    dateTime = GetDateTime(row)
+    return dateTime.hour
+
+def GetDateTime(row):
+    year = int(row['Year'])
+    month = int(row['Month'])
+    day = int(row['Day'])
     time = row['Time']
-    minutes = '%02d' % (time % 100)
-    hour = '%02d' % str(int(math.ceil(time / 100.0)))
-    # dateTime = datetime.strptime('2013 04 03 20 46 UTC', '%Y %m %d %H %M %Z')
-    dateTimeString = year + ' ' + month + ' ' + day + ' ' + 
-    dateTime = datetime.strptime('2013 04 03 20 46 UTC', '%Y %m %d %H %M %Z')
+    minute = time % 100
+    hour = int(math.floor(time / 100.0))
+    # print(str(hour))
+    dateTime = datetime(year, month, day, hour, minute) + timedelta(hours=int(row['TimeZone']))
+    return dateTime
+
+def CalculateFlightDataHour(row):
+    time = row['CRSDepTime']
+    hour = int(math.floor(time / 100.0))
     return hour
+
+def DropDuplicates(df, columns):
+    rowCountBefore = df.shape[0]
+    df.drop_duplicates(keep='first' subset=columns, inplace=True)
+    rowCountAfter = df.shape[0]
+    print('Deleted ' + str(rowCountBefore - rowCountAfter) + ' duplicate rows')
 # def CalculateWeatherDataHour(row):
 #     year = str(row['Year'])
 #     month = '%02d' % int(row['Month'])
